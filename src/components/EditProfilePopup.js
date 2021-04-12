6bac570d418bd1e09cb31ab4294a1207e210558e
Lorna Bogan <lorna.bogan@gmail.com>
2021-04-12 13:04:39 -0500

    profile popup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -7,19 +7,20 @@ function EditProfilePopup(props) {
     const [name, setName] = useState(' ');
     const [description, setDescription] = useState(' ');
     const [errorName, setErrorName] = useState(' ');
+    const [errorDescription, setErrorDescription] = useState(' ');
+    const [disableButton, setDisableButton] = useState(' ');
+    
     // Subscription to the context
     const currentUser = useContext(CurrentUserContext);
 
     function handleChangeName(e){
         setName(e.target.value);
-    }
-
-    function handleNameError(e) {
         setErrorName(e.target.validationMessage);
     }
 
     function handleChangeAbout(e){
         setDescription(e.target.value);
+        setErrorDescription(e.target.validationMessage);
     }
 
 
@@ -52,23 +53,23 @@ function EditProfilePopup(props) {
             onClose={props.onClose}
             onSubmit = {handleSubmit}
             >
+
             <input id = "profile-name" type='text'
             name='name' 
             value = {name || ''}
-            className="popup__input popup__input_type_name" 
+            className={`popup__input popup__input_type_name ${errorName && 'popup__input_type_error'}`}
             onChange={handleChangeName} 
-            error = {handleNameError}
             placeholder='Jacques Cousteau' 
             required maxLength="40" minLength="2"
             />
-            <span id="profile-name-error" className = "popup__error"></span>
+            <span id="profile-name-error" className ='popup__error'>{errorName}</span>
 
             <input id = "profile-text" type='text' 
-            name='about' className='popup__input popup__input_type_job' 
+            name='about' className={`popup__input popup__input_type_job ${errorDescription && 'popup__input_type_error'}`}
             value = {description || ''}
             onChange={handleChangeAbout} placeholder='Explorer' 
             required maxLength="200" minLength="2"/>
-            <span id="profile-text-error" className = "popup__error"></span>
+            <span id="profile-text-error" className = "popup__error">{errorDescription}</span>
         </PopupWithForm>
     );
 }
